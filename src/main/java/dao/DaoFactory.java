ceb798a50321f6a03b49447950fac2b8f1407f9c
DESKTOP-N4EBOK4\Dilshan <dilshanperera200325712934@gmail.com>
2024-01-09 18:46:27 +0530

    dao package initialize

---
+++ src/main/java/dao/DaoFactory.java
@@ -0,0 +1,27 @@
+package dao;
+
+
+import dao.custom.impl.UsersDaoImpl;
+//import dao.custom.impl.ItemDaoImpl;
+import dao.util.DaoType;
+
+import static dao.util.BoType.USERS;
+
+public class DaoFactory {
+    private static DaoFactory daoFactory;
+
+    private DaoFactory(){
+
+    }
+    public static DaoFactory getInstance(){
+        return daoFactory!=null? daoFactory:(daoFactory=new DaoFactory());
+    }
+
+    public <T extends SuperDao>T getDao(DaoType type){
+        switch (type){
+            case USERS: return(T) new UsersDaoImpl();
+            //case ITEM: return (T) new ItemDaoImpl();
+        }
+        return null;
+    }
+}
