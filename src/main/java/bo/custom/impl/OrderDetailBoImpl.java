48e9ae51d3e5e5bf370dc62488693234e0a060d4
DESKTOP-N4EBOK4\Dilshan <dilshanperera200325712934@gmail.com>
2024-02-05 14:58:19 +0530

    create order detailbo class

---
+++ src/main/java/bo/custom/impl/OrderDetailBoImpl.java
@@ -0,0 +1,32 @@
+package bo.custom.impl;
+
+import bo.custom.OrderDetailBo;
+import dao.DaoFactory;
+import dao.custom.OrderDetailsDao;
+import dao.util.DaoType;
+import dto.OrderDetailDto;
+import entity.OrderDetail;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDetailBoImpl implements OrderDetailBo{
+    private OrderDetailsDao orderDetailDao = DaoFactory.getInstance().getDao(DaoType.ORDERDETAIL);
+    @Override
+    public List<OrderDetailDto> allOrderDetails() throws SQLException, ClassNotFoundException {
+        List<OrderDetail> entityList = orderDetailDao.getAll();
+        List<OrderDetailDto> list = new ArrayList<>();
+
+        for (OrderDetail orderDetail:entityList) {
+            list.add(new OrderDetailDto(
+                    orderDetail.getOrders().getOrderId(),
+                    orderDetail.getItem().getItemCode(),
+                    orderDetail.getAdvancePrice(),
+                    orderDetail.getStatus(),
+                    orderDetail.getIssue()
+            ));
+        }
+        return list;
+    }
+}
